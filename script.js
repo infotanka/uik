f3d8dda534dec35d6c719c20ffac89df5274b2e1
Aleksandr Klimenkov <alx.devel@gmail.com>
2013-09-09 23:52:31 +0400

    migrate to google maps

--- script.js
+++ script.js
@@ -1,72 +1,68 @@
 $(function() {
-    var config = {
-        map: 'map'
-    };
-    ymaps.ready(function() {
-        var map = new ymaps.Map(
-                config.map,
-                {
-                    center: [55.76, 37.64],
-                    zoom: 12
-                },
-        {
-            autoFitToViewport: 'always'
-        }
+    function initialize() {
+        var mapOptions = {
+            center: new google.maps.LatLng(55.76, 37.64),
+            zoom: 10,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        /*
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(55.76, 37.64),
+            map: map,
+            title: 'Hello World!'
+        });
+         */
+        var infoIcon = new google.maps.MarkerImage(
+            "images/info.png",
+            new google.maps.Size(8, 8),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(0, 8)
         );
-
-        map.controls.add('zoomControl', {left: '20px', top: '20px'});
-        map.behaviors.disable('dblClickZoom');
-        map.behaviors.enable('scrollZoom');
-
+        var alertIcon = new google.maps.MarkerImage(
+            "images/alert.png",
+            new google.maps.Size(8, 8),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(0, 8)
+        );
+        var warningIcon = new google.maps.MarkerImage(
+            "images/warning.png",
+            new google.maps.Size(8, 8),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(0, 8)
+        );
         $.get('/uiks.json', function(data) {
-            var isClustered = true;
-            var geoObjects = [];
+            var totalCount = data.length;
+            
             $(data).each(function(idx, uik) {
-                var preset = 'twirl#blueIcon';
+                var icon = infoIcon;
                 var color = '';
-                if (uik.sobyaninPercents < 50) {
-                    preset = 'twirl#blueIcon';
-                    color = 'lightblue';
-                } else if (uik.sobyaninPercents < 51) {
-                    preset = 'twirl#darkorangeIcon';
-                    color = 'orange';
-                } else {
-                    preset = 'twirl#redIcon';
-                    color = 'red';
-                }
-
-                var placemark = new ymaps.Placemark(
-                    [uik.lat, uik.lon],
-                    {
-                        iconContent: uik.total,
-                        hintContent: uik.sobyaninPercents + '%',
-                        clusterCaption: 'УИК ' + uik.uic,
-                        balloonContentBody: '<div style="background-color: ' + color + ';width: 110px;">&nbsp;</div><span>Наблюдателей: ' + uik.total + '</span><br><span>Собянин: ' + uik.sobyaninPercents + '%</span>'
-                    },
-                    {
-                        preset: preset,
-                        balloonAutoPan: true
+                var icon = 'twirl#blueIcon';
+                var color = '';
+                
+                if (uik.sobyaninPercents > 50) {
+                    if (uik.total == 0) {
+                        icon = warningIcon;
+                        color = 'red';
+                    } else {
+                        icon = alertIcon;
+                        color = 'red';
                     }
-                );
-
-                if (isClustered) {
-                    geoObjects[idx] = placemark;
                 } else {
-                    map.geoObjects.add(placemark);
+                    icon = infoIcon;
+                    color = 'lightblue';
                 }
-            });
-            if (isClustered) {
-                var clusterer = new ymaps.Clusterer({clusterDisableClickZoom: true});
-                clusterer.add(geoObjects);
-                map.geoObjects.add(clusterer);
-            }
-            var bounds = map.geoObjects.getBounds();
-            if (bounds) {
-                map.setBounds(bounds, {
-                    checkZoomRange: true,
-                    zoomMargin: [20]
+                
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(uik.lat, uik.lon),
+                    map: map,
+                    icon: icon,
+                    title: uik.sobyaninPercents + '%'
                 });
-            }
-        }, 'json');
-    });
-})
+            });
+        });
+    }
+
+    initialize();
+
+});
