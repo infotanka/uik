b02b0fda9f153c6729298b758ee92a6e1d38dde5
Aleksandr Klimenkov <alx.devel@gmail.com>
2013-09-11 18:56:00 +0400

    redesign

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,4 @@
 $(function() {
-    
-    var threshold = 55;
-    var outdoorThreshhold = 2;
-    
     var map = new UicsMap(
         document.getElementById("map"),
         {
@@ -11,34 +7,10 @@ $(function() {
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
     );
-    
-    function renderThresholdValue() {
-        $('.j-threshold-value').text(threshold);
-        $('.j-outdoor-threshold-value').text(outdoorThreshhold);
-        map.render(threshold, outdoorThreshhold);
-    }
-    
-    $('.j-slider').slider({
-        min: 1,
-        max: 99,
-        value: threshold
-    }).on('slideStop', function(event) {
-        threshold = event.value;
-        renderThresholdValue();
-    });
-
-    $('.j-outdoor-slider').slider({
-        min: 1,
-        max: 9,
-        value: outdoorThreshhold
-    }).on('slideStop', function(event) {
-        outdoorThreshhold = event.value;
-        renderThresholdValue();
-    });
 
     $('.j-close-legend').click(function() {
         $('.j-legend-mobile').hide();
     });
-    
-    renderThresholdValue();
+
+    map.render();
 });
