5a8bb1e43f8b2c825234963dd3e65b5e34ecb25b
Dima Devgru <git@devg.ru>
2013-09-17 00:59:43 +0400

    Попытка сделать переключалку

--- scripts/UicsMap.js
+++ scripts/UicsMap.js
@@ -1,37 +1,63 @@
+var RED = 'rgb(211,56,0)';
+var ORANGE = 'rgb(232,204,0)';
+var YELLOW = 'rgb(234,239,0)';
+var GREEN = 'rgb(109,193,55)';
+
+var uics = [];
+var markers = [];
+
+var getUicColor = function (uic, palette) {
+    if (palette == 'observers') {
+        switch (uic.total) {
+            case 0:
+                return RED;
+            case 1:
+                return ORANGE;
+            case 2:
+                return YELLOW;
+        }
+        return GREEN;
+    }
+    var p = uic.outdoorPercents;
+    if (p > 20) return RED;
+    if (p > 10) return ORANGE;
+    if (p > 5) return YELLOW;
+    return GREEN;
+};
+
+var getUicScale = function (uic) {
+    return Math.sqrt(uic.sobyaninPercents);
+};
+
+var getIcon = function (uic) {
+    var color = getUicColor(uic, 'observers');
+    return {
+        path: google.maps.SymbolPath.CIRCLE,
+        strokeOpacity: 0.2,
+        strokeColor: '#ffffff',
+        strokeWeight: 2,
+        fillColor: color,
+        fillOpacity: 1,
+        scale: getUicScale(uic)
+    };
+};
+
+var setPalette = function (palette) {
+    $('.j-line').removeClass('current');
+    $('.s-' + palette).addClass('current');
+    $(markers).each(function (index, marker) {
+        var icon = getIcon(marker.uik);
+        icon.fillColor = getUicColor(marker.uik, palette);
+        marker.setIcon(icon);
+    });
+};
+
 var UicsMap = function (mapElement, options) {
 
-    var getUicColor = function (uic) {
-        var p = uic.outdoorPercents;
-        if (p > 20) return 'rgb(211,56,0)';
-        if (p > 10) return 'rgb(232,204,0)';
-        if (p > 5) return 'rgb(234,239,0)';
-        return 'rgb(109,193,55)';
-    };
-
-    var getUicScale = function (uic) {
-        return Math.sqrt(uic.sobyaninPercents);
-    };
-
-    var getIcon = function (uic) {
-        var color = getUicColor(uic);
-        return {
-            path: google.maps.SymbolPath.CIRCLE,
-            strokeOpacity: 0.2,
-            strokeColor: '#ffffff',
-            strokeWeight: 2,
-            fillColor: color,
-            fillOpacity: 1,
-            scale: getUicScale(uic)
-        };
-    };
 
     var openedInfoWindow = null;
 
     var map = null;
-
-    var uics = [];
-
-    var markers = [];
 
     var loadingDeferred = new $.Deferred();
 
@@ -70,6 +96,7 @@ var UicsMap = function (mapElement, options) {
                         icon: getIcon(uik),
                         title: uik.sobyaninPercents + '%'
                     });
+                    marker.uik = uik;
 
                     var contentString = '<h5>УИК №' + uik.uic + '</h5><div>Результат С. Собянина: ' + uik.sobyaninPercents + '%</div>';
                     contentString = contentString + '<div>Процент «на дому»: ' + uik.outdoorPercents + '%</div>';
